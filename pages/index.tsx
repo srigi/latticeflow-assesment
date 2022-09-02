bfe48cf439494093fd9e8dbb21238939978006f2
Srigi <srigi@srigi.dev>
2022-09-02 16:54:40 +0200

    Add <Stats> component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,16 @@ import Stats from "src/components/Stats";
 import withPageLayout from "src/layouts/1col";
 
 const IndexPage: NextPageWithLayout = () => {
-  return <Grid />;
+  return (
+    <>
+      <div className="flex gap-8 justify-around mb-8">
+        <Stats target="dot" targetPercentage={25} />
+        <Stats target="small" targetPercentage={33.33} />
+        <Stats target="large" targetPercentage={66.66} />
+      </div>
+      <Grid />
+    </>
+  );
 };
 IndexPage.getLayout = (title) => withPageLayout(title("Welcome"));
 
---
+++ src/components/Stats.tsx
@@ -0,0 +1,72 @@
+import { atom, useAtom } from "jotai";
+import { FunctionComponent, useMemo } from "react";
+
+import dataset from "src/atoms/dataset";
+import selection from "src/atoms/selection";
+
+interface Props {
+  target: "dot" | "large" | "small";
+  targetPercentage: number;
+}
+
+const Stats: FunctionComponent<Props> = ({ target, targetPercentage }) => {
+  const selectedPercentageAtom = useMemo(
+    () =>
+      atom((get) => {
+        const allItems = get(dataset);
+        const selectedItemsIds = get(selection);
+        const selectedTargetItems = allItems
+          .filter((i) => (target === "dot" ? i.dot : i.size === target))
+          .filter((i) => selectedItemsIds.includes(i.id));
+
+        return (
+          Math.round(
+            (selectedTargetItems.length / selectedItemsIds.length) * 100 * 100
+          ) / 100 || 0
+        );
+      }),
+    [target]
+  );
+  const [selectedPercentage] = useAtom(selectedPercentageAtom);
+
+  return (
+    <div className="mb-4 flex-1 max-w-sm">
+      <h3 className="mb-6 ml-4">
+        <strong>{target}</strong>{" "}
+        <span className="text-indigo-500">target</span>: {targetPercentage}%
+      </h3>
+
+      <div className="relative">
+        <div
+          className="absolute bg-gray-700 h-4 w-1 -top-4"
+          style={{
+            left: `${targetPercentage}%`,
+          }}
+        />
+        <div className="bg-gray-700 h-1 -mr-1" />
+        <div
+          className="absolute bg-indigo-500 h-4 w-1 top-1"
+          style={{
+            left: `${selectedPercentage}%`,
+          }}
+        />
+      </div>
+
+      <div className="mt-4">
+        <p>
+          actual: <strong>{selectedPercentage}%</strong>
+        </p>
+        <p>
+          difference:{" "}
+          <strong>
+            {(Math.round((targetPercentage - selectedPercentage) * 100) / 100) *
+              -1}
+            %
+          </strong>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Stats;
